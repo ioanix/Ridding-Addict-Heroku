e42ced78d5902570a30109671dab7bdaa883b6b9
ioanix <ioana_1001@yahoo.com>
2021-06-09 22:08:22 +0300

    project setup

---
+++ src/main/java/ubb/postuniv/riddingaddict/RiddingAddictApplication.java
@@ -0,0 +1,13 @@
+package ubb.postuniv.riddingaddict;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RiddingAddictApplication {
+
+    public static void main(String[] args) {
+
+        SpringApplication.run(RiddingAddictApplication.class, args);
+    }
+}
